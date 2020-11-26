90590fd435c2d2856fb80c688196d013d262feec
刘富豪 <17670613645@163.com>
2020-11-26 19:20:33 +0800

    修改

--- src/views/404.vue
+++ src/views/404.vue
@@ -6,6 +6,7 @@
         <img class="pic-404__child left" src="@/assets/404_images/404_cloud.png" alt="404">
         <img class="pic-404__child mid" src="@/assets/404_images/404_cloud.png" alt="404">
         <img class="pic-404__child right" src="@/assets/404_images/404_cloud.png" alt="404">
+        
       </div>
       <div class="bullshit">
         <div class="bullshit__oops">OOPS!</div>
